8844f06cd84eccca83425ed758f310adac74e38e
RafaelAguilar11 <rafaithaaguilar@gmail.com>
2026-03-08 02:12:25 -0600

    programa 4

--- funciones/Program.cs
+++ funciones/Program.cs
@@ -8,7 +8,7 @@ saludar();
 
 
 
-//Mostrar un nombre en pantalla utilizando una función MostrarNombre().
+//2. Mostrar un nombre en pantalla utilizando una función MostrarNombre().
 void MostrarNombre()
 {
     Console.WriteLine("Rafael");
@@ -17,11 +17,18 @@ void MostrarNombre()
 MostrarNombre();
 
 
-//Mostrar un mensaje en pantalla utilizando una función MostrarMensaje() que imprima “Programación en C#”.
-void MostrarMensaje();
+//3. Mostrar un mensaje en pantalla utilizando una función MostrarMensaje() que imprima “Programación en C#”.
+void MostrarMensaje()
 {
     Console.WriteLine("Programación en C#");
 }
 
 MostrarMensaje();
 
+//4. Imprimir una línea separadora utilizando una función Linea() que muestre “----------------”.
+void linea()
+{
+    Console.WriteLine("----------------");
+}
+
+linea();
